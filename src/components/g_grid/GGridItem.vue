27cb84dd30a42613ed805553dc55bc2c4b02eedd
garethfuller <hello@garethfuller.me>
2019-04-23 08:15:58 +0100

    Fix grid vert spacing

--- src/components/g_grid/GGridItem.vue
+++ src/components/g_grid/GGridItem.vue
@@ -1,5 +1,5 @@
 <template lang="html">
-  <div :class="[`g-grid-item sm:px-${spacing} px-0 mb-${vertSpacing} md:mb-${spacing*2}`, classes]">
+  <div :class="[`g-grid-item sm:px-${spacing} px-0 mb-${vertSpacing} md:mb-${vertSpacing*2}`, classes]">
     <slot></slot>
   </div>
 </template>
