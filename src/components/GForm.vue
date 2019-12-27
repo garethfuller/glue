2f27cc377b7ec0d6397dd0252ccc16aae808f8f6
garethfuller <hello@garethfuller.me>
2019-12-27 20:49:29 +0000

    Recursive validate form

--- src/components/GForm.vue
+++ src/components/GForm.vue
@@ -18,15 +18,24 @@ export default {
 
   methods: {
     validate() {
-      this.$children.forEach(input => {
-        if (typeof input.validate === 'function') {
-          if (['g-checkbox', 'g-switch'].includes(input.$options._componentTag)) {
-            input.validate(input.$props.checked)
-          } else {
-            input.validate(input.$props.value)
-          }
-        }
+      this.recursiveValidate(this.$children)
+    },
+
+    recursiveValidate(children) {
+      children.forEach(child => {
+        this.validateInput(child)
+        if (child.$children.length > 0) this.recursiveValidate(child.$children)
       })
+    },
+
+    validateInput(input) {
+      if (typeof input.validate === 'function') {
+        if (['g-checkbox', 'g-switch'].includes(input.$options._componentTag)) {
+          input.validate(input.$props.checked)
+        } else {
+          input.validate(input.$props.value)
+        }
+      }
     }
   }
 }
