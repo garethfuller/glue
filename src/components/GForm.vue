bf7d9ae357718fd6e3cf495c2e6a71f97fa45d0f
garethfuller <hello@garethfuller.me>
2019-12-27 22:17:52 +0000

    Ensure boolean return on form validation

--- src/components/GForm.vue
+++ src/components/GForm.vue
@@ -16,9 +16,18 @@ export default {
     method: { type: String, default: 'post' },
   },
 
+  data() {
+    return {
+      errors: []
+    }
+  },
+
   methods: {
     validate() {
+      this.errors = []
       this.recursiveValidate(this.$children)
+      if (this.errors.length > 0) return false
+      return true
     },
 
     recursiveValidate(children) {
@@ -32,9 +41,17 @@ export default {
       if (typeof input.validate === 'function') {
         if (['g-checkbox', 'g-switch'].includes(input.$options._componentTag)) {
           input.validate(input.$props.checked)
+          this.handleInputErrors(input.$data.errors)
         } else {
           input.validate(input.$props.value)
+          this.handleInputErrors(input.$data.errors)
         }
+      }
+    },
+
+    handleInputErrors(errors) {
+      if (errors && errors.length > 0) {
+        this.errors.push(...errors)
       }
     }
   }
