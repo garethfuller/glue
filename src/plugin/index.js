9b2b479c7a2b5f350ab4301b60bfa6149edc2434
Gareth Fuller <gareth.a.f@gmail.com>
2020-04-28 21:17:02 +0100

    Update plugin export

--- src/plugin/index.js
+++ src/plugin/index.js
@@ -2,24 +2,24 @@ import * as components from './components';
 import * as mixins from './mixins';
 import * as directives from './directives';
 
-const Glue = {
-  install(Vue) {
-    Object.keys(components).forEach((component) => {
-      Vue.component(components[component].name, components[component]);
-    });
+const install = function(Vue) {
+  Object.keys(components).forEach((component) => {
+    Vue.component(components[component].name, components[component]);
+  });
 
-    Object.keys(mixins).forEach((mixin) => {
-      Vue.mixin(mixins[mixin]);
-    });
+  Object.keys(mixins).forEach((mixin) => {
+    Vue.mixin(mixins[mixin]);
+  });
 
-    Object.keys(directives).forEach((directive) => {
-      Vue.directive(directives[directive].name, directives[directive].directive);
-    });
-  },
-};
-
-if (typeof window !== 'undefined' && window.Vue) {
-  window.Vue.use(Glue);
+  Object.keys(directives).forEach((directive) => {
+    Vue.directive(directives[directive].name, directives[directive].directive);
+  });
 }
 
-export default Glue;
+if (typeof window !== 'undefined' && window.Vue) {
+  install(window.Vue);
+}
+
+export default {
+  install
+}
